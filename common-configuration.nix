efa284b0ede664180cfa3f6daaad17fab53521ca
Curtis D'Alves <curtis.dalves@gmail.com>
2021-01-31 23:59:55 -0500

    fixed picom (wasn't tangling)

--- common-configuration.nix
+++ common-configuration.nix
@@ -340,6 +340,17 @@
              polybar top 2>/home/dalvescb/.polybar_error.log &
       '';
     };
+    services.picom = {
+        enable = true;
+        activeOpacity = "1.0";
+        inactiveOpacity = "0.8";
+        backend = "glx";
+        fade = true;
+        fadeDelta = 5;
+        opacityRule = [ "100:name *= 'i3lock'" ];
+        shadow = true;
+        shadowOpacity = "0.75";
+    };
     gtk = {
       enable = true;
       iconTheme = {
