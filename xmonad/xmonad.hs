2d90814522524cc13c63a592ba4680d309b8c4fc
Curtis D'Alves <curtis.dalves@gmail.com>
2022-02-19 13:24:06 -0500

    swapped window switch keys in xmonad because monitor order got swapped

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -310,7 +310,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = Map.fromList $
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_i, xK_o, xK_u] [0..]
+        | (key, sc) <- zip [xK_i, xK_u, xK_o] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
     -- ++
