c298a55cd73b2b77c3d27d77adb4ad620ff58927
Curtis D'Alves <curtis.dalves@gmail.com>
2022-05-13 12:18:26 -0400

    updated alacritty config

--- common-configuration.nix
+++ common-configuration.nix
@@ -460,35 +460,20 @@
       };
       font.size = 14.0;
       import = [ "~/nixconfig/alacritty/dracula.yml" ];
-      # colors = {
-      #   primary = {
-      #     background = "0x1a1b26";
-      #     foreground = "0xa9b1d6";
-      #   };
-      #   normal = {
-      #     black =  "0x32344a";
-      #     red =    "0xf7768e";
-      #     green =  "0x9ece6a";
-      #     yellow = "0xe0af68";
-      #     blue =   "0x7aa2f7";
-      #     magenta ="0xad8ee6";
-      #     cyan =   "0x449dab";
-      #     white =  "0x787c99";
-      #   };
-      #   bright = {
-      #     black =  "0x444b6a";
-      #     red =    "0xff7a93";
-      #     green =  "0xb9f27c";
-      #     yellow = "0xff9e64";
-      #     blue =   "0x7da6ff";
-      #     magenta ="0xbb9af7";
-      #     cyan =   "0x0db9d7";
-      #   };
-      # };
       key_bindings = [
         {
           key = "Escape";
           action = "ToggleViMode";
+        }
+        {
+          key = "LBracket";
+          mods = "Control";
+          action = "ToggleViMode";
+        }
+        {
+          key = "N";
+          mods = "Control|Shift";
+          action = "SpawnNewInstance";
         }
       ];
     };
